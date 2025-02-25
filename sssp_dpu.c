e5cdf9d2da88d9756f727544956a76d4934ba93a
Krishna <56349351+KrishnaK1238@users.noreply.github.com>
2025-02-25 21:00:38 +0100

    made final changes
    dpu and cpu outputs match, can handle large graphs, graphs are being distributed across dpus

--- sssp_dpu.c
+++ sssp_dpu.c
@@ -3,18 +3,23 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <barrier.h>
 
-#define MAX_EDGES 20000
-#define MAX_VERTICES 5000
+#define MAX_EDGES 2000000
+#define MAX_VERTICES 5000000
+
 
 typedef struct {
     int u, v, weight;
+        int32_t padding; 
 } Edge;
 
 __mram_noinit Edge edges[MAX_EDGES];
 __mram_noinit int distances[MAX_VERTICES];
 __host int NUM_VERTICES;
-__host int NUM_EDGES;              
+__host int NUM_EDGES;
+
+
 
 int main() {
     bool updated = true;
@@ -24,21 +29,25 @@ int main() {
     	printf("Number of tasklets: %u \n", NR_TASKLETS);
     	}
 
-for (int iter = 0; iter < NUM_VERTICES - 1; iter++) {  
-    updated = false;
+    int edges_per_tasklet = NUM_EDGES / NR_TASKLETS;
+    int start = tasklet_id * edges_per_tasklet;
+    int end = (tasklet_id == NR_TASKLETS - 1) ? NUM_EDGES : (start + edges_per_tasklet);
 
-    for (int i = 0; i < NUM_EDGES; i++) { 
-        int u = edges[i].u;
-        int v = edges[i].v;
-        int weight = edges[i].weight;
+    for (int iter = 0; iter < NUM_VERTICES - 1; iter++) {  
+        updated = false;
 
-        if (distances[u] != 1000000 && distances[u] + weight < distances[v]) {
-            distances[v] = distances[u] + weight;
-            updated = true;
+        for (int i = start; i < end; i++) { 
+            int u = edges[i].u;
+            int v = edges[i].v;
+            int weight = edges[i].weight;
+
+            if (distances[u] != 1000000 && distances[u] + weight < distances[v]) {
+                distances[v] = distances[u] + weight;
+                updated = true;
+            }
         }
-    }
 
-    if (!updated) {  
+        if (!updated) {  
         break;
     }
 }
--- sssp_host.c
+++ sssp_host.c
@@ -1,15 +1,15 @@
 #include <assert.h>
 #include <dpu.h>
-#include <dpu_log.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <sys/time.h>
 
 #define INFINITY 1000000
 
+
 typedef struct {
-    int u, v, weight;
+    int32_t u, v, weight;
+        int32_t padding;  
 } Edge;
 
 double get_time_in_seconds() {
@@ -39,34 +39,13 @@ void read_graph(const char *filename, int *num_vertices, int *num_edges, Edge **
     fclose(file);
 }
 
-double cpu_sssp(const char *filename, int* cpu_distances) {
-    int num_vertices, num_edges;
-    Edge *edges;
-
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("Failed to open file");
-        exit(EXIT_FAILURE);
-    }
-    fscanf(file, "%d %d", &num_vertices, &num_edges);
-    edges = (Edge *)malloc(num_edges * sizeof(Edge));
-    for (int i = 0; i < num_edges; i++) {
-        fscanf(file, "%d %d %d", &edges[i].u, &edges[i].v, &edges[i].weight);
-    }
-    fclose(file);
-
-    if (!cpu_distances) {
-        perror("Failed to allocate memory for distances");
-        free(edges);
-        exit(EXIT_FAILURE);
-    }
-
+double cpu_sssp(int num_vertices, int num_edges, Edge *edges, int *cpu_distances) {
     for (int i = 0; i < num_vertices; i++) {
         cpu_distances[i] = INFINITY;
     }
     cpu_distances[0] = 0;
 
-    clock_t start_time = clock();
+    double start_time = get_time_in_seconds();
 
     for (int i = 0; i < num_vertices - 1; i++) {
         for (int j = 0; j < num_edges; j++) {
@@ -80,12 +59,8 @@ double cpu_sssp(const char *filename, int* cpu_distances) {
         }
     }
 
-    clock_t end_time = clock();
-    double total_execution_time_ms = (double)(end_time - start_time) * 1000 / CLOCKS_PER_SEC;
-
-
-    free(edges);
-    return total_execution_time_ms;
+    double end_time = get_time_in_seconds();
+    return (end_time - start_time) * 1000; // Execution time in ms
 }
 
 int main(int argc, char **argv) {
@@ -97,26 +72,22 @@ int main(int argc, char **argv) {
     const char *graph_file = argv[1];
     struct dpu_set_t set, dpu;
 
-    
-
-    int num_vertices, num_edges;
+    int32_t num_vertices, num_edges;
     Edge *edges;
 
     read_graph(graph_file, &num_vertices, &num_edges, &edges);
-    int* cpu_distances = (int *)malloc(num_vertices * sizeof(int));
-   
-    int *dpu_distances = (int *)malloc(num_vertices * sizeof(int));
-    	
-    printf("\nRunning SSSP on CPU...\n");
-    double cpu_execution_time_ms = cpu_sssp(graph_file, cpu_distances);
-    printf("CPU Total Execution Time: %.3f ms\n", cpu_execution_time_ms);
+    int32_t *cpu_distances = (int *)malloc(num_vertices * sizeof(int));
+    int32_t *dpu_distances = (int *)malloc(num_vertices * sizeof(int));
 
-  
-    if (!dpu_distances) {
+    if (!cpu_distances || !dpu_distances) {
         perror("Failed to allocate memory for distances");
         free(edges);
         return EXIT_FAILURE;
     }
+
+    printf("\nRunning SSSP on CPU...\n");
+    double cpu_execution_time_ms = cpu_sssp(num_vertices, num_edges, edges, cpu_distances);
+    printf("CPU Execution Time: %.3f ms\n", cpu_execution_time_ms);
 
     for (int i = 0; i < num_vertices; i++) {
         dpu_distances[i] = INFINITY;
@@ -124,32 +95,70 @@ int main(int argc, char **argv) {
     dpu_distances[0] = 0;
 
     double start_time = get_time_in_seconds();
+    int32_t NR_DPUS = 64;
 
-    uint32_t nr_dpus = 63;
-    DPU_ASSERT(dpu_alloc(nr_dpus, "backend=simulator", &set));
+    DPU_ASSERT(dpu_alloc(NR_DPUS, "backend=simulator", &set));
     DPU_ASSERT(dpu_load(set, "./sssp_dpu", NULL));
-    DPU_ASSERT(dpu_get_nr_dpus(set, &nr_dpus));
-    printf("Number of DPUs allocated: %u\n", nr_dpus);
-    printf("number of vertices: %u, number of edges: %u\n", num_vertices, num_edges);
+
+    int32_t edges_per_dpu = (num_edges + NR_DPUS - 1) / NR_DPUS;
+    int dpu_id = 0;
 
     double transfer_start_time = get_time_in_seconds();
 
-    int dpu_id = 0;
     DPU_FOREACH(set, dpu) {
-   
-    DPU_ASSERT(dpu_copy_to(dpu, "edges", 0, edges, num_edges * sizeof(Edge)));
-    DPU_ASSERT(dpu_copy_to(dpu, "distances", 0, dpu_distances, num_vertices * sizeof(int)));
-    DPU_ASSERT(dpu_copy_to(dpu, "NUM_VERTICES", 0, &num_vertices, sizeof(num_vertices)));
-    DPU_ASSERT(dpu_copy_to(dpu, "NUM_EDGES", 0, &num_edges, sizeof(num_edges)));
+        int32_t start_idx = dpu_id * edges_per_dpu;
+        int32_t end_idx = (start_idx + edges_per_dpu < num_edges) ? (start_idx + edges_per_dpu) : num_edges;
+        int32_t partition_size = end_idx - start_idx;
+        printf("DPU %d is handling edges [%d - %d] (%d edges)\n", dpu_id, start_idx, end_idx - 1, partition_size);
+        DPU_ASSERT(dpu_copy_to(dpu, "edges", 0, &edges[start_idx], partition_size * sizeof(Edge)));
+        DPU_ASSERT(dpu_copy_to(dpu, "NUM_VERTICES", 0, &num_vertices, sizeof(num_vertices)));
+        DPU_ASSERT(dpu_copy_to(dpu, "NUM_EDGES", 0, &partition_size, sizeof(partition_size)));
 
-    dpu_id++;
+        dpu_id++;  // 🔥 FIXED: Increment `dpu_id` inside loop
+    }
+    DPU_FOREACH(set, dpu) {
+        DPU_ASSERT(dpu_copy_to(dpu, "distances", 0, dpu_distances, num_vertices * sizeof(int)));
     }
 
     double transfer_end_time = get_time_in_seconds();
     double cpu_to_dpu_time_ms = (transfer_end_time - transfer_start_time) * 1000;
 
     double dpu_execution_start_time = get_time_in_seconds();
-    DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
+    for (int iter = 0; iter < num_vertices - 1; iter++) {
+        DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
+
+        bool any_updates = false;
+        int temp_distances[num_vertices];
+
+        DPU_FOREACH(set, dpu) {
+            DPU_ASSERT(dpu_copy_from(dpu, "distances", 0, temp_distances, num_vertices * sizeof(int32_t)));
+
+            for (int i = 0; i < num_vertices; i++) {
+                if (temp_distances[i] < dpu_distances[i]) {
+                    dpu_distances[i] = temp_distances[i];
+                    any_updates = true;
+                }
+            }
+        }
+
+        if (!any_updates) {
+            break;
+        }
+
+        DPU_FOREACH(set, dpu) {
+            DPU_ASSERT(dpu_copy_to(dpu, "distances", 0, dpu_distances, num_vertices * sizeof(int32_t)));
+        }
+    }
+    printf("\nFirst 10 distances:\n");
+    for (int i = 0; i < 10 && i < num_vertices; i++) {
+        printf("Vertex %d: CPU = %d, DPU = %d", i, cpu_distances[i], dpu_distances[i]);
+        if (cpu_distances[i] != dpu_distances[i]) {
+            printf("  [Mismatch!]");
+        }
+        printf("\n");
+    }
+
+
     double dpu_execution_end_time = get_time_in_seconds();
     double dpu_execution_time_ms = (dpu_execution_end_time - dpu_execution_start_time) * 1000;
 
@@ -162,42 +171,29 @@ int main(int argc, char **argv) {
 
     double end_time = get_time_in_seconds();
     double total_execution_time_ms = (end_time - start_time) * 1000;
-    
-    printf("\nCPU Computed Shortest Distances\n");
-    for (int i = 0; i <= 10 && i < num_vertices; i++) {
-        printf("Vertex %d: %d\n", i, cpu_distances[i]);
-    }
 
-    printf("\nDPU Computed Shortest Distances\n");
-    for (int i = 0; i <= 10 && i < num_vertices; i++) {
-        printf("Vertex %d: %d\n", i, dpu_distances[i]);
+    printf("\nComparison of CPU and DPU Results:\n");
+    int correct = 0, incorrect = 0;
+    for (int i = 0; i < num_vertices; i++) {
+        if (cpu_distances[i] == dpu_distances[i]) {
+            correct++;
+        } else {
+            incorrect++;
+        }
     }
-    
-   
+    printf("Matching distances: %d\n", correct);
+    printf("Mismatched distances: %d\n", incorrect);
 
     printf("\nMetrics:\n");
-    printf("Total Execution Time: %.3f ms\n", total_execution_time_ms);
+    printf("CPU Execution Time: %.3f ms\n", cpu_execution_time_ms);
+    printf("DPU Execution Time: %.3f ms\n", total_execution_time_ms);
     printf("CPU to DPU Transfer Time: %.3f ms\n", cpu_to_dpu_time_ms);
-    printf("DPU Execution Time: %.3f ms\n", dpu_execution_time_ms);
     printf("DPU to CPU Transfer Time: %.3f ms\n", dpu_to_cpu_time_ms);
 
-    size_t memory_transferred = (num_edges * sizeof(Edge)) + (num_vertices * sizeof(int)) + (2 * sizeof(int));
-    size_t total_dpu_memory = 64 * 1024 * 1024;
     size_t memory_used = (num_edges * sizeof(Edge)) + (num_vertices * sizeof(int));
-    double memory_utilization = (double)memory_used / total_dpu_memory * 100;
+    double memory_utilization = (double)memory_used / (64 * 1024 * 1024) * 100;
 
-    printf("Memory Transferred: %.2f KB\n", memory_transferred / 1024.0);
     printf("DPU Memory Utilization: %.6f%%\n", memory_utilization);
-
-    printf("\nComparison:\n");
-    printf("CPU Total Execution Time: %.3f ms\n", cpu_execution_time_ms);
-    printf("DPU Total Execution Time: %.3f ms\n", total_execution_time_ms);
-
-    printf("\nDPU Log Output:\n");
-    DPU_FOREACH(set, dpu) {
-        DPU_ASSERT(dpu_log_read(dpu, stdout));
-    }
-
     DPU_ASSERT(dpu_free(set));
     free(edges);
     free(dpu_distances);
